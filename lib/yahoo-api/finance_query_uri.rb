f97d8551b84c019b9f7bb13b1f0878015c5fc88b
James Whinfrey <james@conceptric.co.uk>
2012-07-26 13:39:19 +0100

    Uses an inner class to define URI class methods and up versions the gem

--- lib/yahoo-api/finance_query_uri.rb
+++ lib/yahoo-api/finance_query_uri.rb
@@ -6,36 +6,43 @@ module Yahoo
       FINANCE_DATABASE = "yahoo.finance.quotes"
       DATATABLE = "&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 
-      class QueryURI                 
-        def self.build(tickers)
-          validate_tickers(tickers)
-          URI(build_query_uri(tickers))
-        end        
+      class QueryURI
+        
+        class <<self                 
 
-        private  
-  
-        def self.validate_tickers(tickers)
-          if tickers.class != Array
-            raise ArgumentError, "Tickers must be supplied in an Array"
-          elsif tickers.empty? || tickers.include?("")
-            raise ArgumentError, "At least one ticker must be supplied"
+          def build(tickers)
+            validate_tickers(tickers)
+            URI(build_query_uri(tickers))
           end        
-        end
-  
-        def self.build_query_uri(tickers)
-          base_query =  API_QUERY_URL + select_data_from + FINANCE_DATABASE + 
-              for_these_symbols(tickers)
-          query = URI.encode(base_query) + "&format=json" + DATATABLE
-        end     
 
-        def self.for_these_symbols(tickers)
-          " where symbol in (\"#{tickers.join("\", \"")}\")"    
-        end
+          private  
   
-        def self.select_data_from
-          "select symbol, Ask, Bid from "        
-        end  
+          def validate_tickers(tickers)
+            if tickers.class != Array
+              raise ArgumentError, "Tickers must be supplied in an Array"
+            elsif tickers.empty? || tickers.include?("")
+              raise ArgumentError, "At least one ticker must be supplied"
+            end        
+          end
+  
+          def build_query_uri(tickers)
+            base_query =  API_QUERY_URL + select_data_from + FINANCE_DATABASE + 
+                for_these_symbols(tickers)
+            query = URI.encode(base_query) + "&format=json" + DATATABLE
+          end     
+
+          def for_these_symbols(tickers)
+            " where symbol in (\"#{tickers.join("\", \"")}\")"    
+          end
+  
+          def select_data_from
+            "select symbol, Ask, Bid from "        
+          end 
+
+        end
+
       end
+
     end
   end
 end
